9f731d9c2ab19a1dd5402ce2a612b419318663c1
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-03-28 10:25:51 -0500

    add sql import fumnctions

---
+++ classification/acquire.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import env
+
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+def get_titanic_data():
+    return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
+
+def get_iris_data():
+    return pd.read_sql('SELECT 	s.species_name, m.* FROM species AS s JOIN measurements AS m ON  s.species_id = m.species_id', get_connection('iris_db'))
+
+titanic_df = get_titanic_data()
+iris_df = get_iris_data()
+
+print(titanic_df.head())
+print(iris_df.head())
